59bf2bd132091495d13bfbb12cfe3761639e4b4e
SANKAR2167 <snakarkulandaivel@gmail.com>
2022-11-25 11:29:41 +0530

    new

--- src/App.js
+++ src/App.js
@@ -14,7 +14,22 @@ export default App;
 function Header() {
   return(
     <div className='header'>
-      
+      <ul className='navbar-nav'>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Start BootStrap</a>
+        </li>
+        <div className='navbar-menu'>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Shop</a>
+        </li>
+        </div>
+      </ul>
     </div>
   )
 }
