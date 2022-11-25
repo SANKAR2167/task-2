085af483b792bfa5981af69c43d14bf4c5fce1de
SANKAR2167 <snakarkulandaivel@gmail.com>
2022-11-25 12:06:44 +0530

    gs

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default App;
 function Header() {
   return(
     <div className='header'>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
         <a class="navbar-brand" href="#">Shoping Website</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
