0cbf261dbf1b21d978d39920cd4e6f2d4c057a07
SANKAR2167 <snakarkulandaivel@gmail.com>
2022-11-25 11:35:21 +0530

    s

--- src/App.js
+++ src/App.js
@@ -14,22 +14,32 @@ export default App;
 function Header() {
   return(
     <div className='header'>
-      <ul className='navbar-nav'>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Start BootStrap</a>
-        </li>
-        <div className='navbar-menu'>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Shop</a>
-        </li>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Shoping Website</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                Shop
+              </a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">All Products</a>
+                <a class="dropdown-item" href="#">Popular Items</a>
+                <a class="dropdown-item" href="#">New Arrivals</a>
+              </div>
+            </li>
+          </ul>
         </div>
-      </ul>
+      </nav>
     </div>
   )
 }
